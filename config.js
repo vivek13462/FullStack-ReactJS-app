387343f7ca5420eb48a114a6185b31710ef14f1c
vivek.13462 <vivek.13462@gmail.com>
2017-12-31 23:20:23 -0800

    updated package.json and created different modules for components

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-console.log('config')
+const env = process.env;
+
+export const nodeEnv = env.NODE_ENV || 'development';
+
+export const logStars = function(message) {
+  console.info('**********');
+  console.info(message);
+  console.info('**********');
+};
+
+export default {
+  port: env.PORT || 8080
+};
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Header =({ message }) => {
+  return (
+        <h2 className="Header text-center">
+        {message}
+        </h2> 
+      );
+};
+
+Header.propTypes = {
+    message: React.PropTypes.string
+};
+
+export default Header;
